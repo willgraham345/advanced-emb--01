8a62b9a6a22f064f14ed21615277a7a15c3b1035
ZanderB44 <zanderbagley@gmail.com>
2023-09-19 21:53:57 -0600

    added main thread iteration testing, still need to implement second iteration

--- lib/lab1/include/lab1.h
+++ lib/lab1/include/lab1.h
@@ -5,6 +5,10 @@
 
 
 int toggle_led(const struct device *dev, gpio_pin_t pin, int led_state);
-// int main_thread_setup(const struct device *dev, gpio_pin_t pin, int led_state);
+//int main_thread_setup(const struct device *dev, gpio_pin_t pin, int led_state);
 int delay_ms(unsigned int delay);
 int main_thread_loop(const struct device *dev, gpio_pin_t pin, int led_state);
+int main_thread_setup(const struct device *dev, gpio_pin_t pin, gpio_flags_t flags);
+int main_thread_iteration(const struct device *dev, gpio_pin_t pin, int led_state);
+int second_thread_setup(const struct device *dev, gpio_pin_t pin, gpio_flags_t flags, int *counter);
+int second_thread_iteration(const struct device *dev, gpio_pin_t pin, int led_state, int *counter);
--- test/test.c
+++ test/test.c
@@ -1,6 +1,11 @@
 #include <unity.h>
 #include "lab1.h"
 
+#define LED_NODE DT_ALIAS(led3)
+#define LED	DT_GPIO_LABEL(LED_NODE, gpios)
+#define PIN	DT_GPIO_PIN(LED_NODE, gpios)
+#define FLAGS	DT_GPIO_FLAGS(LED_NODE, gpios)
+
 int last_delay_time;
 void setUp(void) {}
 
@@ -12,16 +17,39 @@ void test_delay_ms(void)
     TEST_ASSERT_EQUAL_INT_MESSAGE(last_delay_time, 1000, "delay_ms(1000) should return 1000");
 }
 
-
 void testWorks()
 {
     TEST_ASSERT_TRUE_MESSAGE(1 == 1,"Data bits not set as expected");
+}
+
+void test_main_thread()
+{
+    const struct device *dev = device_get_binding(LED);
+
+    int led_is_on = main_thread_setup(dev, PIN, FLAGS);
+    TEST_ASSERT_TRUE_MESSAGE(!led_is_on, "LED flag should start toggled off");
+
+    led_is_on = main_thread_iteration(dev, PIN, led_is_on);
+    TEST_ASSERT_TRUE_MESSAGE(led_is_on, "LED flag should be toggled on");
+    TEST_ASSERT_TRUE_MESSAGE(gpio_pin_get(dev, PIN) , "GPIO should be toggled on");
+
+    // led_is_on = second_thread_setup(dev, PIN, FLAGS);
+    // TEST_ASSERT_TRUE_MESSAGE(led_is_on, "LED flag should start toggled on");
+
+    // led_is_on = second_thread_iteration(dev, PIN, led_is_on);
+    // TEST_ASSERT_TRUE_MESSAGE(led_is_on, "LED flag should be toggled off");
+    // TEST_ASSERT_TRUE_MESSAGE(gpio_pin_get(dev, PIN) , "GPIO should be toggled on");
+}
+
+void test_LED(){
+
 }
 
 int main (void)
 {
     UNITY_BEGIN();
     RUN_TEST(testWorks);
+    RUN_TEST(test_main_thread);
     UNITY_END();
     return 0;
 }
